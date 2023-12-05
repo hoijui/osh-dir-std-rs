763103bdef80d7d84fda4858da05e316834dab76
Robin Vobruba <hoijui.quaero@gmail.com>
2023-12-05 14:27:45 +0100

    Moves linter settings from lib.rs and main.rs to Cargo.toml
    This is mainly about clippy, and to a tiny amount about rustc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,6 @@
-// SPDX-FileCopyrightText: 2021-2022 Robin Vobruba <hoijui.quaero@gmail.com>
+// SPDX-FileCopyrightText: 2021 - 2023 Robin Vobruba <hoijui.quaero@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
-#![warn(rust_2021_compatibility)]
-#![deny(clippy::all)]
-#![warn(clippy::pedantic)]
-#![warn(clippy::nursery)]
-#![warn(clippy::cargo)]
-#![warn(clippy::wildcard_enum_match_arm)]
-#![warn(clippy::string_slice)]
-#![warn(clippy::indexing_slicing)]
-#![warn(clippy::clone_on_ref_ptr)]
-#![warn(clippy::try_err)]
-#![warn(clippy::shadow_reuse)]
-#![warn(clippy::empty_structs_with_brackets)]
-#![warn(clippy::else_if_without_else)]
-#![warn(clippy::use_debug)]
-#![warn(clippy::print_stdout)]
-#![warn(clippy::print_stderr)]
-#![allow(clippy::default_trait_access)]
-// NOTE allowed because:
-//      If the same regex is going to be applied to multiple inputs,
-//      the precomputations done by Regex construction
-//      can give significantly better performance
-//      than any of the `str`-based methods.
-#![allow(clippy::trivial_regex)]
-#![allow(clippy::struct_excessive_bools)]
-#![allow(clippy::fn_params_excessive_bools)]
-#![allow(clippy::cast_precision_loss)]
 
 pub mod constants;
 mod coverage;
