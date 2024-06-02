7b7059ac6cba3feae73ed2b438987a8d1cb71a5e
Robin Vobruba <hoijui.quaero@gmail.com>
2024-06-02 12:43:32 +0100

    Adds a must_use [fix]

--- lib/src/tree.rs
+++ lib/src/tree.rs
@@ -82,6 +82,7 @@ impl<'a> Default for Node<'a> {
 ///
 /// If a Record does not have at least one path part, or
 /// if a combined path regex turns out ot be malformed.
+#[must_use]
 pub fn create<'a>(std_raw: &'a format::DirStd) -> (RNode, Vec<RNode>) {
     let mut pp_recs: Vec<(Vec<String>, &'a format::Rec<'static>)> = std_raw
         .records
